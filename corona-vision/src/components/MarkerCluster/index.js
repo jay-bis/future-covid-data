d7d08f6b16388e122183c077caeef8277b7a4d1b
Jack Biscupski <jbiscupski@uiowa.edu>
2020-03-27 17:26:04 -0500

    add text to tooltip, get rid of number on cluster

--- corona-vision/src/components/MarkerCluster/index.js
+++ corona-vision/src/components/MarkerCluster/index.js
@@ -1,11 +1,31 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import L from 'leaflet';
 import "leaflet.markercluster/dist/leaflet.markercluster";
 import "leaflet.markercluster/dist/MarkerCluster.css";
-import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 import { useLeaflet } from 'react-leaflet';
 
-const mcg = L.markerClusterGroup();
+import '../../MarkerCluster.Default.css';
+
+const mcg = L.markerClusterGroup({
+    iconCreateFunction: function(cluster) {
+        const childCount = cluster.getChildCount();
+
+        var c = ' marker-cluster-';
+        if (childCount < 10) {
+            c += 'small';
+        } else if (childCount < 100) {
+            c += 'medium';
+        } else {
+            c += 'large';
+        }
+
+        return new L.DivIcon({ className: 'marker-cluster' + c, iconSize: new L.Point(50, 50)});
+    }
+});
+
+const tooltipMsgCurrent = (cases, date) => {
+    return `${date}: ${cases.toString()} current cases`
+};
 
 const MarkerCluster = props => {
 
@@ -13,11 +33,11 @@ const MarkerCluster = props => {
 
     useEffect(() => {
         mcg.clearLayers();
-        props.markers.forEach(( { position, currentCaseCount }) => (
+        props.markers.forEach(({ position, currentCaseCount, date }) => (
             L.marker(new L.LatLng(position.lat, position.lng))
         )
         .addTo(mcg)
-        .bindPopup(currentCaseCount.toString())
+        .bindTooltip(tooltipMsgCurrent(currentCaseCount, date))
         )
     map.addLayer(mcg);
     }, [props.markers, map]);

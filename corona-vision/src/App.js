eb6b0ac46d4502d864746017bb667157aa1faec7
Jack Biscupski <jbiscupski@uiowa.edu>
2020-03-27 18:08:18 -0500

    add sidebar bg, bootstrap cdn

--- corona-vision/src/App.js
+++ corona-vision/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import GlobalMap from './components/GlobalMap';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <GlobalMap />
+      <Sidebar />
     </div>
   );
 }
---
+++ corona-vision/src/components/Sidebar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Sidebar.css';
+
+const Sidebar = props => {
+
+
+    return (
+        <div className="sidenav">
+
+        </div>
+    )
+};
+
+export default Sidebar;

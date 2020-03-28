db2fcbfa4a049211bbafd333bc95abfe4017926b
Jack Biscupski <jbiscupski@uiowa.edu>
2020-03-28 14:25:55 -0500

    add legend, change test data, colored markers

--- corona-vision/src/components/MarkerCluster/index.js
+++ corona-vision/src/components/MarkerCluster/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import L from 'leaflet';
 import "leaflet.markercluster/dist/leaflet.markercluster";
 import "leaflet.markercluster/dist/MarkerCluster.css";
@@ -8,8 +8,8 @@ import '../../MarkerCluster.Default.css';
 
 const mcg = L.markerClusterGroup();
 
-const tooltipMsgCurrent = (cases, date) => {
-    return `${date}: ${cases.toString()} current cases`
+const tooltipMsg = date => {
+    return `Case on: ${date}`
 };
 
 const MarkerCluster = props => {
@@ -18,13 +18,43 @@ const MarkerCluster = props => {
 
     useEffect(() => {
         mcg.clearLayers();
-        props.markers.forEach(({ position, currentCaseCount, date }) => (
-            L.marker(new L.LatLng(position.lat, position.lng))
+        props.markers.forEach(({ position, symptomatic, date }) => (
+            L.marker(new L.LatLng(position.lat, position.lng), {
+                icon: new L.Icon({iconUrl: symptomatic 
+                    ? 
+                    'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png'
+                    :
+                    'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png'
+                })
+            })
         )
         .addTo(mcg)
-        .bindTooltip(tooltipMsgCurrent(currentCaseCount, date))
+        .bindTooltip(tooltipMsg(date))
         )
     map.addLayer(mcg);
+
+    var legend = L.control({position: 'topleft'});
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info-legend');
+        const labels = ['<strong>Legend</strong>'];
+        const categories = ['Symptomatic','Asymptomatic'];
+
+        for (var i = 0; i < categories.length; i++) {
+
+                div.innerHTML += 
+                labels.push(
+                    categories[i] === 'Symptomatic' 
+                    ? "<p>Symptomatic User: </p><img src='https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png' />"
+                    : "<p>Asymptomatic User: </p><img src='https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png' />"
+                );
+
+            }
+            div.innerHTML = labels.join('<br>');
+        return div;
+    };
+    legend.addTo(map);
+
     }, [props.markers, map]);
 
     return null;

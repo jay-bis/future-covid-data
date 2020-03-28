60468829db23879ae0e819d648ea0bd61015e112
Jes Ford <jes.ford@recursionpharma.com>
2020-03-28 17:17:02 -0600

    Make efficient-er

--- models/aggregate.py
+++ models/aggregate.py
@@ -13,18 +13,18 @@ pf = ParquetFile(file)
 # for experimenting purposes
 df = pf.read_row_group(0).to_pandas()
 
-# add lat and lon columns to dataframe
+# calculate lat and lon
 unique_geohash = df[['geo_hash']].drop_duplicates()
 lat_lon = pd.DataFrame(
     unique_geohash.geo_hash.apply(pygeohash.decode).to_list(),
     columns=['lat', 'lon'])
 lat_lon['geo_hash'] = unique_geohash.values
-df = pd.merge(df, lat_lon, on='geo_hash')
 
-# descretize onto a grid
-lon_ref, lat_ref = df.lon.mean(), df.lat.mean()  # reference points
+# get distances in km in both directions
+lon_ref, lat_ref = lat_lon.lon.mean(), lat_lon.lat.mean()  # reference points
 geod = Geod(ellps='WGS84')
-df["lon_dist_km"] = df.lon.map(
+lat_lon["lon_dist_km"] = lat_lon.lon.map(
     lambda x: geod.inv(lon_ref, lat_ref, x, lat_ref)[2] / 1000)
-df["lat_dist_km"] = df.lat.map(
+lat_lon["lat_dist_km"] = lat_lon.lat.map(
     lambda x: geod.inv(lon_ref, lat_ref, lon_ref, x)[2] / 1000)
+df = pd.merge(df, lat_lon, on='geo_hash')

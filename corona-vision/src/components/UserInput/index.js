0017261d3a09a805855f547ac1bddf1aff3e0669
Jack Biscupski <jbiscupski@uiowa.edu>
2020-03-28 21:27:20 -0500

    started UserInput, added react-bootstrap, react-places-autocomplete, and react-hook-form

---
+++ corona-vision/src/components/UserInput/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UserInput = props => {
+
+
+    return (
+        <div>
+            <button type="button">Click Me</button>
+        </div>
+    )
+}
+
+export default UserInput;

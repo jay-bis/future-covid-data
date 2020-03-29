cef089e713b084ed8da4c3aaf83d700c7690b886
Jack Biscupski <jbiscupski@uiowa.edu>
2020-03-29 01:51:17 -0500

    address searching, lists

--- corona-vision/src/components/UserInput/index.js
+++ corona-vision/src/components/UserInput/index.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import { Button, Modal, ListGroup } from 'react-bootstrap';
+
+import '../AddressForm';
+import AddressForm from '../AddressForm';
 
 const UserInput = props => {
 
+    const [show, setShow] = React.useState(false);
+    const [addrList, setAddrList] = React.useState([]);
+
+    const handleClose = () => {
+        setShow(false);
+        setAddrList([]);
+    }
+    const handleShow = () => setShow(true);
+
+    const handleSubmit = () => {
+        console.log('hello');
+    }
 
     return (
         <div>
-            <button type="button">Click Me</button>
+            <Button type="button" onClick={handleShow}>Enter Input</Button>
+        
+            <Modal 
+                show={show} 
+                onHide={handleClose}
+                size="lg"
+                centered
+                >
+                <Modal.Header closeButton>
+                    <Modal.Title>Enter your last known locations (up to 14 days ago)</Modal.Title>
+                </Modal.Header>
+                    <Modal.Body>
+                        <AddressForm 
+                            setParentAddrList={setAddrList}
+                        />
+                        <ListGroup>
+                            {addrList.map(addr => (
+                                <ListGroup.Item>{addr}</ListGroup.Item>
+                            ))}
+                        </ListGroup>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>Close</Button>
+                        <Button variant="primary" onClick={handleSubmit}>Submit</Button>
+                    </Modal.Footer>
+            </Modal>
         </div>
     )
 }
